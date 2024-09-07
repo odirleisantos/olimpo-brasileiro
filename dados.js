b1824b796b8f69a7c66e220f18da639d205746d1
odirleisantos <87076773+odirleisantos@users.noreply.github.com>
2024-09-07 00:34:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "César Cielo",
+        descricao: "O Rei da Velocidade na Piscina. César Cielo Filho é o nome que ecoa na história da natação brasileira como o único campeão olímpico da modalidade. Sua conquista histórica aconteceu nos Jogos Olímpicos de Pequim 2008, onde ele dominou a prova dos 50 metros livre, estabelecendo um novo recorde olímpico.",
+        link: "https://pt.wikipedia.org/wiki/C%C3%A9sar_Cielo",
+        tags: "natação natacao cesar cielo nadador piscina"
+    },
+    {
+        titulo: "Ítalo Ferreira",
+        descricao: "Rei das Ondas. Ítalo Ferreira fez história ao conquistar a primeira medalha de ouro do surfe para o Brasil nos Jogos Olímpicos de Tóquio 2020. Sua performance impressionante colocou o surfe brasileiro no topo do pódio olímpico.",
+        link: "https://pt.wikipedia.org/wiki/%C3%8Dtalo_Ferreira",
+        tags: "surfe italo prancha onda italo oceano praia"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Fada do Esporte. Rebeca Andrade encantou o mundo com sua performance na ginástica artística nos Jogos Olímpicos de Tóquio 2020. A brasileira conquistou a medalha de ouro no solo, tornando-se a primeira ginasta brasileira a conquistar uma medalha de ouro individual em uma Olimpíada.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica rebeca ginasta"
+    },
+    {
+        titulo: "Martine Grael e Kahena Kunze",
+        descricao: "Rainhas dos Mares. Martine Grael e Kahena Kunze fizeram história ao conquistar a medalha de ouro na vela nos Jogos Olímpicos de Tóquio 2020. A dupla brasileira demonstrou grande técnica e determinação, dominando a competição.",
+        link: "https://pt.wikipedia.org/wiki/Martine_Grael",
+        tags: "vela martine grael kahena kunze barco mar"
+    },
+    {
+        titulo: "Adhemar Ferreira da Silva",
+        descricao: "Uma lenda do atletismo brasileiro, Adhemar conquistou duas medalhas de ouro consecutivas no salto triplo, em 1952 (Helsinque) e 1956 (Melbourne), consagrando-se como um dos maiores atletas da história do país. Sua marca e sua elegância nos saltos ainda são lembradas com admiração.",
+        link: "https://pt.wikipedia.org/wiki/Adhemar_Ferreira_da_Silva",
+        tags: "atletismo 1952 1956 lenda"
+    },
+    {
+        titulo: "Thiago Braz",
+        descricao: "O salto com vara brasileiro ganhou um novo protagonista com Thiago Braz. Em uma final emocionante, ele quebrou o recorde olímpico e conquistou o ouro nas Olimpíadas do Rio, tornando-se um dos maiores ídolos do esporte brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Thiago_Braz",
+        tags: "vara"
+    }
+];
